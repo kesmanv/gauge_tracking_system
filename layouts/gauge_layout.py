42d0af52c568b2034e0ef3c21813f432358c3ce2
Kesman Valdes <kesman.v@gmail.com>
2020-12-22 16:03:49 -0500

    added input fields to Add Gauge tab

--- layouts/gauge_layout.py
+++ layouts/gauge_layout.py
@@ -13,7 +13,7 @@ from dash_extensions import Download
 import pandas as pd
 from dash.dependencies import Input, Output, State
 from app import app
-from layouts import plots, cards, navigation_bar, filters
+from layouts import plots, cards, navigation_bar, filters, tabs
 from database import transforms as tf
 import plotly.graph_objects as go
 
@@ -37,83 +37,6 @@ blue_btn_style = {
     ,'padding': '0 30px'
     #,'font-size': '11px'
 }
-#####################################################################################################################
-####################################### Gauge App - Table of records ################################################
-#####################################################################################################################
-
-
-# Table records
-gauge_table = dash_table.DataTable(
-                                            
-                                id='gauge-table' 
-                                ,data = tf.df.to_dict('rows')
-                                ,columns=[
-                                        {'name':'Plant'
-                                        ,'id':'Plant'}
-                                        ,{'name': 'Gauge ID'
-                                        ,'id': 'Gage ID'
-                                        }, {
-                                                'name':'Description'
-                                                ,'id': 'Description'
-                                        }, {
-                                                'name':'Gauge S/N'
-                                                ,'id': 'Gage S/N'
-                                        }, {
-                                                'name':'Asset No'
-                                                ,'id': 'Asset No'
-                                        }, {
-                                                'name':'Model No.'
-                                                ,'id': 'Model No.'
-                                        },{
-                                                'name':'Current Location'
-                                                ,'id': 'Current Location'
-                                        }, {
-                                                'name':'Manufacturer'
-                                                ,'id': 'Manufacturer'
-                                        }, {
-                                                'name':'Last Calibration By'
-                                                ,'id': 'Last Calibrated By'
-                                        } , {
-                                                'name':'Last Calibration Date'
-                                                ,'id': 'Last Cal Date'
-                                        }, {
-                                                'name':'Next Due Date'
-                                                ,'id': 'Next Due Date'
-                                                ,'type': 'datetime'
-                                        }
-                                        ]
-                                        ,page_size=20
-                                        ,style_cell_conditional=[
-                                                                {'if': {'column_id':'Plant'}, 'width':'60px', 'textAlign':'center'}
-                                                                ,{'if': {'column_id':'Gage ID'}, 'width':'100px', 'textAlign':'center'}
-                                                                ,{'if': {'column_id':'Description'}, 'width':'280px', 'textAlign':'center'}
-                                                                ,{'if': {'column_id':'Gage S/N'}, 'width':'120px', 'textAlign':'center'}
-                                                                ,{'if': {'column_id':'Asset No'}, 'width':'80px', 'textAlign':'center'}
-                                                                ,{'if': {'column_id':'Model No.'}, 'width':'80px', 'textAlign':'center'}
-                                                                ,{'if': {'column_id':'Current Location'}, 'width':'140px', 'textAlign':'center'}
-                                                                ,{'if': {'column_id':'Manufacturer'}, 'width':'100px', 'textAlign':'center'}
-                                                                ,{'if': {'column_id':'Last Calibrated By'}, 'width':'180px', 'textAlign':'center'}
-                                                                ,{'if': {'column_id':'Last Cal Date'}, 'width':'170px', 'textAlign':'center'}
-                                                                ,{'if': {'column_id':'Next Due Date'}, 'width':'130px', 'textAlign':'center'}
-                                        ]
-                                        ,style_header={'fontWeight':'bold'}
-                                        ,sort_action='native' 
-                                        ,cell_selectable=True
-                                        #,editable=True
-                                        #,filter_action='native'
-                                        # ,style_data_conditional=[{
-                                        #         'if':{
-                                        #                 'filter_query':'{Due Soon} contains "Yes"'
-                                        #         }, 'column_id':'Plant', 'backgroundColor':'#ffeeb2'
-                                        # }]
-
-                                                
-                                         
-)
-
-# gauge_table_updated = dash_table.DataTable(
-#         id = 'gauge-table-updated'
-# )
 
 #####################################################################################################################
 ####################################### Gauge App - Main Layout #####################################################
@@ -210,9 +133,10 @@ layout = html.Div([
 def tab_gauge_content(active_tab):
     if active_tab=='tab-gauge-records':
         download_btn = html.Div([html.Button("Download records", id="download-btn", style=white_btn_style), Download(id="download")]) 
-        return gauge_table, download_btn
+        #print('tab_gauge_content')
+        return tabs.gauge_table, download_btn
     elif active_tab=='tab-add-gauge':
-        return html.H2('New Gauge placeholder'), html.Br()
+        return tabs.add_gauge_layout, html.Br()
     elif active_tab=='tab-gauge-studies':
         return html.H2('Gauge studies placeholder'), html.Br()
     elif active_tab=='tab-gauge-capabilities':
@@ -226,6 +150,7 @@ def tab_gauge_content(active_tab):
     
 )
 def make_gauge_tbl_editable(value):
+    #print('make table editable')
     if value == []:
         style = {'display':'none'}
         return False, style
@@ -246,7 +171,7 @@ def make_gauge_tbl_editable(value):
 def tb_gauge_filters(plant, mfg, g_type, due):
     filters_input = {'Plant': plant, 'Manufacturer':mfg, 'Type':g_type, 'Due Soon': due}
     filtered_dict = {k:v for k,v in filters_input.items() if len(v)!=0}
-
+    #print('tb gauge filters')
     if len(filtered_dict)==0:
         return tf.df.to_dict('rows')
     else:
@@ -268,6 +193,7 @@ def tb_gauge_filters(plant, mfg, g_type, due):
 )
 
 def set_gauge_mfg_options(plant, gauge_type): 
+    #print('set gauge mfg options')
     if len(plant) == 0:
 
         mfg_options = tf.drop_options(tf.gauge_manufacturer)
@@ -294,6 +220,7 @@ def set_gauge_mfg_options(plant, gauge_type):
     ]
 )
 def generate_csv(n_clicks, data):
+    #print('generate csv')
     if n_clicks is not None and tf.current_clicks != n_clicks:
         s = io.StringIO()
         pd.DataFrame.from_dict(data).to_csv(s, index=False)
@@ -318,12 +245,12 @@ def generate_csv(n_clicks, data):
     ]
 )
 def update_gauge_table(active_cell, data, data_previous, editable, clicks):
-        
+    #print('update gauge table') # - using this to troubleshoot missing output id warnings 
     if editable == ['Y']:
         if active_cell is not None:
             tf.changed_cells['row_id'].append(active_cell['row_id'])
             tf.changed_cells['column_id'].append(active_cell['column_id'])
-            
+            print(tf.changed_cells)
         return ['tracking']   
     else:
         return ['nothing tracked']
---
+++ layouts/tabs.py
@@ -0,0 +1,209 @@
+import dash_table
+import pandas as pd
+from database import transforms as tf
+import dash_core_components as dcc
+import dash_bootstrap_components as dbc
+import dash_html_components as html
+from database import transforms as ts
+
+cols = ['Gage ID', 'Description', 'Gage S/N', 'Asset No', 'Current Location', 'Manufacturer'
+        ,'Last Calibrated By', 'Last Cal Date', 'Next Due Date']
+
+
+# Table records
+gauge_table = dash_table.DataTable(
+                                            
+                                id='gauge-table' 
+                                ,data = tf.df.to_dict('rows')
+                                ,columns=[
+                                        {'name':'Plant'
+                                        ,'id':'Plant'}
+                                        ,{'name': 'Gauge ID'
+                                        ,'id': 'Gage ID'
+                                        }, {
+                                                'name':'Description'
+                                                ,'id': 'Description'
+                                        }, {
+                                                'name':'Gauge S/N'
+                                                ,'id': 'Gage S/N'
+                                        }, {
+                                                'name':'Asset No'
+                                                ,'id': 'Asset No'
+                                        }, {
+                                                'name':'Model No.'
+                                                ,'id': 'Model No.'
+                                        },{
+                                                'name':'Current Location'
+                                                ,'id': 'Current Location'
+                                        }, {
+                                                'name':'Manufacturer'
+                                                ,'id': 'Manufacturer'
+                                        }, {
+                                                'name':'Last Calibration By'
+                                                ,'id': 'Last Calibrated By'
+                                        } , {
+                                                'name':'Last Calibration Date'
+                                                ,'id': 'Last Cal Date'
+                                        }, {
+                                                'name':'Next Due Date'
+                                                ,'id': 'Next Due Date'
+                                                ,'type': 'datetime'
+                                        }
+                                        ]
+                                        ,page_size=20
+                                        ,style_cell_conditional=[
+                                                                {'if': {'column_id':'Plant'}, 'width':'60px', 'textAlign':'center'}
+                                                                ,{'if': {'column_id':'Gage ID'}, 'width':'100px', 'textAlign':'center'}
+                                                                ,{'if': {'column_id':'Description'}, 'width':'280px', 'textAlign':'center'}
+                                                                ,{'if': {'column_id':'Gage S/N'}, 'width':'120px', 'textAlign':'center'}
+                                                                ,{'if': {'column_id':'Asset No'}, 'width':'80px', 'textAlign':'center'}
+                                                                ,{'if': {'column_id':'Model No.'}, 'width':'80px', 'textAlign':'center'}
+                                                                ,{'if': {'column_id':'Current Location'}, 'width':'140px', 'textAlign':'center'}
+                                                                ,{'if': {'column_id':'Manufacturer'}, 'width':'100px', 'textAlign':'center'}
+                                                                ,{'if': {'column_id':'Last Calibrated By'}, 'width':'180px', 'textAlign':'center'}
+                                                                ,{'if': {'column_id':'Last Cal Date'}, 'width':'170px', 'textAlign':'center'}
+                                                                ,{'if': {'column_id':'Next Due Date'}, 'width':'130px', 'textAlign':'center'}
+                                        ]
+                                        ,style_header={'fontWeight':'bold'}
+                                        ,sort_action='native' 
+                                        ,cell_selectable=True
+                                        #,editable=True
+                                        #,filter_action='native'
+                                        # ,style_data_conditional=[{
+                                        #         'if':{
+                                        #                 'filter_query':'{Due Soon} contains "Yes"'
+                                        #         }, 'column_id':'Plant', 'backgroundColor':'#ffeeb2'
+                                        # }]                                       
+)
+
+#############################################################################################################################################
+############################################### Add New Gauge ###############################################################################
+#############################################################################################################################################
+
+input_style_label = {
+        'margin-top': '20px'
+        ,'margin-botton': '20px'
+        ,'margin-left': '60px'
+        }
+input_style_field = {
+        'margin-top': '14px'
+        ,'margin-botton': '14px'
+        }
+
+input_style_label_date = {
+        'margin-top': '37px'
+        ,'margin-botton': '37px'
+        ,'margin-left': '60px'
+        }
+
+input_style_date = {
+        'margin-top': '14px'
+        ,'margin-botton': '14px'
+        #,'height': '10px'
+        }
+
+add_gauge_layout = html.Div(
+        [
+                dbc.Row(
+                        [
+                                dbc.Col(
+                                        [
+                                                html.P('Gauge ID', style=input_style_label)
+                                                ,html.P('Plant', style=input_style_label)
+                                                ,html.P('Description', style=input_style_label)
+                                                ,html.P('Status', style=input_style_label)
+                                                ,html.P('Gauge S/N', style=input_style_label)
+                                                ,html.P('Asset No.', style=input_style_label)
+                                                ,html.P('Model No.', style=input_style_label)
+                                                ,html.P('Type', style=input_style_label)
+                                                ,html.P('Unit of Meas', style=input_style_label)
+                                                ,html.P('Current Location', style=input_style_label)
+                                                ,html.P('Supplier Code', style=input_style_label)
+                                                ,html.P('Cost', style=input_style_label)                                                
+                                                ,html.P('Purchase Date', style=input_style_label)
+                                                ,html.P('Manufacturer', style=input_style_label)
+                                        ]
+                                        ,width = 1.5
+                                )
+                                ,dbc.Col(
+                                        [
+                                                dcc.Input(id='gauge-id-input', type = 'text', style=input_style_field)
+                                                #,dcc.Input(id='gauge-plant-input', type = 'text', style=input_style_field)
+                                                ,dcc.Dropdown(
+                                                        id='plant'
+                                                        ,options = [{'label': i, 'value': i} for i in ts.plant_names]
+                                                        ,placeholder='Select a plant'
+                                                        ,multi=True
+                                                        ,value=[]
+                                                        ,style={'margin-top': '14px','margin-botton': '14px', 'width': '180px'}
+                                                )
+                                                ,dcc.Input(id='gauge-description-input', type = 'text', style=input_style_field)
+                                                ,dcc.Input(id='gauge-status-input', type = 'text', style=input_style_field)
+                                                ,dcc.Input(id='gauge-serial-input', type = 'text', style=input_style_field)
+                                                ,dcc.Input(id='gauge-asset-input', type = 'text', style=input_style_field)
+                                                ,dcc.Input(id='gauge-model-input', type = 'text', style=input_style_field)
+                                                ,dcc.Input(id='gauge-type-input', type = 'text', style=input_style_field)
+                                                ,dcc.Input(id='gauge-uom-input', type = 'text', style=input_style_field)
+                                                ,dcc.Input(id='gauge-location-input', type = 'text', style=input_style_field)
+                                                ,dcc.Input(id='gauge-supplier-code-input', type = 'text', style=input_style_field)
+                                                ,dcc.Input(id='gauge-cost-input', type = 'text', style=input_style_field)
+                                                ,dcc.Input(id='gauge-purchase-date-input', type = 'text', style=input_style_field)
+                                                ,dcc.Input(id='gauge-manufacturer-input', type = 'text', style=input_style_field)
+                                        ]
+                                        ,width=2
+                                )
+                                ,dbc.Col(
+                                        [
+                                                
+                                                html.P('LabelCode', style=input_style_label)
+                                                ,html.P('Calibrator', style=input_style_label)
+                                                ,html.P('Last Calibration By', style=input_style_label)
+                                                ,html.P('Calibration Frequency (Months)', style=input_style_label)
+                                                ,html.P('Last Calibration Date', style=input_style_label_date)
+                                                ,html.P('Next Due Calibration Date', style=input_style_label_date)
+                                                ,html.P('Next R&R Date', style=input_style_label_date)
+                                                ,html.P('Last R&R Date', style=input_style_label_date)
+                                                ,html.P('Last R&R Result', style=input_style_label)
+                                                ,html.P('Resolution', style=input_style_label)
+                                                ,html.P('Pos. Tolerance', style=input_style_label)
+                                                ,html.P('Neg. Tolerance', style=input_style_label)
+                                        ]
+                                        ,width = 1.5
+                                )
+                                ,dbc.Col(
+                                        [
+                                                
+                                                dcc.Input(id='gauge-label-code-input', type = 'text', style=input_style_field)
+                                                ,dcc.Input(id='gauge-calibrator-input', type = 'text', style=input_style_field)
+                                                ,dcc.Input(id='gauge-last-calibration-by-input', type = 'text', style=input_style_field)
+                                                ,dcc.Input(id='gauge-calibration-freq-input', type = 'text', style=input_style_field)
+                                                ,dcc.DatePickerSingle(id='gauge-last-calibration-date-input', style=input_style_date)
+                                                ,dcc.DatePickerSingle(id='gauge-last-rr-date-input',  style=input_style_date)
+                                                ,dcc.DatePickerSingle(id='gauge-next-rr-date-input',  style=input_style_date)
+                                                ,dcc.DatePickerSingle(id='gauge-rr-date-input',  style=input_style_date)
+                                                ,dcc.Input(id='gauge-last-rr-result-input',  style=input_style_date)
+                                                ,dcc.Input(id='gauge-resolution-input', type = 'number', style=input_style_field)
+                                                ,dcc.Input(id='gauge-pos-tolerance-input', type = 'number', style=input_style_field)
+                                                ,dcc.Input(id='gauge-neg-tolerance-input', type = 'number', style=input_style_field) 
+                                        ]
+                                        ,width =2 
+                                )
+                                ,dbc.Col(
+                                        [
+                                                html.A('Notes')
+                                                ,html.Br()
+                                                ,dcc.Input(id='gauge-notes-input', size='65', type = 'text', style=input_style_field)
+                                        ]
+                                )
+                        ]
+                )
+                #,dbc.Row([]) # Notes
+                
+                
+
+
+
+        ]
+
+)
+
